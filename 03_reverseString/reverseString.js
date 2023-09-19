8374c95bb9a24107efe3c47182762e6b96e869de
Linden Kyaw <kyawlinden@gmail.com>
2023-09-19 15:34:03 -0700

    Add logic to reverse a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+const reverseString = function (inputString) {
+  if (inputString === "") return "";
 
+  // use a pointer to point to the last character
+  // of the input string and concatenate each
+  // letter individually
+
+  let reversedString = "";
+  const length = inputString.length;
+  for (let i = length - 1; i >= 0; i--) {
+    reversedString = reversedString + inputString.charAt(i);
+  }
+
+  return reversedString;
 };
 
 // Do not edit below this line
