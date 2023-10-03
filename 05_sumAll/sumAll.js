c646ee5e9d4adc89c3e15dd59a82001c415b619c
Linden Kyaw <kyawlinden@gmail.com>
2023-10-02 20:08:40 -0700

    Add solution for sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+  // returns the sum between two numbers (inclusive)
+  // should consider what happens if max arg is < min arg
 
+  if (max < 0 || min < 0 || !Number.isInteger(min) || !Number.isInteger(max)) {
+    return "ERROR";
+  }
+
+  if (max < min) {
+    const temp = max;
+    max = min;
+    min = temp;
+  }
+
+  let sum = min;
+  for (let i = min + 1; i <= max; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
