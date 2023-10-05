1247131b75e423a345bc0f0ff2a241cf8e0cc600
Linden Kyaw <kyawlinden@gmail.com>
2023-10-04 19:59:43 -0700

    Add solution for getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,14 @@
-const getTheTitles = function() {
+const getTheTitles = function (books) {
+  // loop through the books array and append the titles to another array
+  // then return that array
 
+  let titles = [];
+
+  for (const book of books) {
+    titles.push(book.title);
+  }
+
+  return titles;
 };
 
 // Do not edit below this line
