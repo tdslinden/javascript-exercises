53130704039c1ab8f9d60e16568c7830fe0c88c1
Linden Kyaw <kyawlinden@gmail.com>
2023-10-04 19:56:17 -0700

    Add recursive solution for fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (typeof num !== "string") {
+    num = +num;
+    if (typeof num !== "number") return "OOPS";
+  }
+  if (num < 0) return "OOPS";
+  if (num === 0) return 0;
+  if (num <= 2) return 1;
 
+  if (num > 2) {
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  }
 };
 
 // Do not edit below this line
